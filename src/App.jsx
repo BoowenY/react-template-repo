ecbf44aebb30cba0be8dec176b1bad85e608b1e5
an-ger <aiboowen@gmail.com>
2021-03-09 11:32:52 +0800

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { memo } from 'react'
+import routes from '../src/router'
+import {HashRouter} from 'react-router-dom'
+import {renderRoutes} from 'react-router-config'
+import Discover from './pages/Discover'
+import Yorn from './pages/Yorn'
 
 export default memo(function App() {
     return (
-        <div>
-            App
-        </div>
+        <HashRouter>
+            The first route：<Discover/>
+            The mounted route：{renderRoutes(routes)}
+            The second route:<Yorn/>
+        </HashRouter>
     )
 })
